ca9a5d32cc4aed144694f9fc1aacc5457816af67
LookPC <niaz.look@yandex.ru>
2020-10-16 13:04:47 +0300

    add point 19 kod "Console.Write("Для выхода нажмите любую клавишу...");"

--- Mingaliev/Program.cs
+++ Mingaliev/Program.cs
@@ -10,6 +10,14 @@ namespace Mingaliev
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите две переменных чтобы найти их сумму: \na = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int b = int.Parse(Console.ReadLine());
+            int sum = a + b;
+            Console.WriteLine("Сумма переменных 'a' и 'b' равна: {0}", sum);
+            Console.Write("Для выхода нажмите любую клавишу...");
+            Console.ReadLine();
         }
     }
 }
